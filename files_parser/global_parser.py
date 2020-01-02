ec4bc4d5e6de5a6d731a6c17bf035e4d823da0b5
vovapasko <vovapasko@gmail.com>
2020-01-03 01:07:18 +0200

    Need to remaster excel parser function

--- files_parser/global_parser.py
+++ files_parser/global_parser.py
@@ -3,8 +3,15 @@ import logging
 from files_parser.parser_tools import extract_data_from_file
 
 
+def handle_data(excel_data, converted_data):
+
+
+    pass
+
+
 def parse(filename_dir, filenames, data):
     converted_data = extract_data_from_file(filename_dir, filenames)
     logging.info(f"The data was parsed from {len(filenames)} files")
     for i in range(len(filenames)):
         logging.info(f"{i + 1}. {filenames[i]}")
+    handle_data(data, converted_data)
