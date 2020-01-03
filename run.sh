eaf3c9461c862052ec9a8989f2408325198ce36c
vovapasko <vovapasko@gmail.com>
2020-01-03 12:56:33 +0200

    Provided bash script for running file

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source venv/bin/activate
+# you can change the name of the file you want to exectute
+# generally it is main.py, but you can run test, for example
+python main.py
+deactivate
