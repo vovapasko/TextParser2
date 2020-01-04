be511afa0938cfc91075a8cf09d80a26572e9d5d
vovapasko <vovapasko@gmail.com>
2020-01-05 01:40:54 +0200

    Updated first_start.sh

--- first_start.sh
+++ first_start.sh
@@ -3,3 +3,9 @@
 echo "Creating Python3.6 venv..."
 python3.6 -m venv venv
 echo "venv Created!"
+echo "Updated pip and installing all libs from requirements.txt"
+source venv/bin/activate
+pip install --upgrade pip
+pip install -r requirements.txt
+deactivate
+echo "Script finished successfully"
