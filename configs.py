46a5d9ccbb7138e26023ca9172682d656ee7ac07
vovapasko <vovapasko@gmail.com>
2020-01-22 17:06:31 +0200

    tested progam with custom folder

--- configs.py
+++ configs.py
@@ -6,7 +6,8 @@ import pathlib
 
 home_project_directory = pathlib.Path(__file__).parent
 test_data_directory = home_project_directory / "test_data"
-path_to_files = test_data_directory / "danger_data"
+# path_to_files = test_data_directory / "danger_data"
+path_to_files = pathlib.Path("c:/custom_folder/danger_data")
 logs_dir = home_project_directory / "logs"
 res_dir = home_project_directory / "res"
 res_filename = res_dir / "Dovidnyk_NPP_Irmis.xls"
