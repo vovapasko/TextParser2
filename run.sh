5f0c378d8b3bcb0a938b52db368581515d9b2038
vovapasko <vovapasko@gmail.com>
2020-01-23 12:54:17 +0200

    Deleted run.sh and first_start.sh. Renamed test_run.sh to run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-source venv/bin/activate
-# you can change the name of the file you want to exectute
-# generally it is main.py, but you can run test, for example
+venv_dir="venv/"
+
+
+if [ ! -d "$venv_dir" ]; then 
+	echo "venv doesn't exist"
+	echo "Creating venv"
+	python3.6 -m venv venv
+	echo "venv created!"
+	source venv/bin/activate
+	echo "Updating pip..."
+	pip install --upgrade pip
+	echo "pip updated!"
+	echo "installing requirements"
+	pip install -r requirements.txt
+	echo "Requirements are installed!"
+else
+	source venv/bin/activate
+fi
+
 python main.py
 deactivate
+
