0d5e309ee2c1853c6276d5976472f9d51905666a
vovapasko <vovapasko@gmail.com>
2020-01-26 12:01:24 +0200

    Remastered run.sh

--- run.sh
+++ run.sh
@@ -2,23 +2,22 @@
 
 venv_dir="venv/"
 
-
-if [ ! -d "$venv_dir" ]; then 
-	echo "venv doesn't exist"
-	echo "Creating venv"
-	python3.6 -m venv venv
-	echo "venv created!"
-	source venv/bin/activate
-	echo "Updating pip..."
-	pip install --upgrade pip
-	echo "pip updated!"
-	echo "installing requirements"
-	pip install -r requirements.txt
-	echo "Requirements are installed!"
+if [ ! -d "$venv_dir" ]; then
+  echo "venv doesn't exist"
+  echo "Creating venv"
+  python3.6 -m venv venv
+  echo "venv created!"
+  source venv/bin/activate
+  echo "Updating pip..."
+  pip install --upgrade pip
+  echo "pip updated!"
+  echo "installing requirements"
+  pip install -r requirements.txt
+  echo "Requirements are installed!"
+  source venv/bin/activate
 else
-	source venv/bin/activate
+  source venv/bin/activate
 fi
 
 python main.py
 deactivate
-
